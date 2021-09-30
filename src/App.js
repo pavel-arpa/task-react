86d6e3d832efee2debc7ccbdf4fc985dab20da9a
Pavel Arepev <protocoltype7@gmail.com>
2021-09-30 20:37:15 +0300

    Styles was removed in js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
 import ColoredUserpic from "./components/ColoredUserpic";
 
 function App() {
   return (
     <div className="App">
-      <ColoredUserpic />
+        <ColoredUserpic
+            src='./avatar.png'
+            size={150}
+            margin={4}
+            backgroundColor='red'
+            colors={['green', 'blue']}
+            hoverColors={['blue', 'green']}
+            colorWidth={6}
+        />
     </div>
   );
 }
--- src/components/ColoredUserpic.js
+++ src/components/ColoredUserpic.js
@@ -1,12 +1,41 @@
-import s from './ColoredUserpic.module.css'
-import avatar from '../avatar.png'
+import {useState} from 'react'
+import avatar from "../avatar.png"
 
 function ColoredUserpic(props) {
+    const [bgColor, setBgColor] = useState({f: props.colors[1], s: props.colors[0]})
+
+    const Avatar = {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "0 auto",
+        marginTop: "100px",
+        background: `linear-gradient(90deg, ${bgColor.f}, ${bgColor.s})`,
+        borderRadius: "50%",
+        width: `${props.size}px`,
+        height: `${props.size}px`,
+    }
+    const AvatarBg = {
+        width: `${props.size - props.colorWidth * 2}px`,
+        height: `${props.size - props.colorWidth * 2}px`,
+        backgroundColor: props.backgroundColor,
+        borderRadius: "50%"
+    }
+    const AvatarImg = {
+        background: "blue",
+        borderRadius: "50%",
+        width: `${props.size - props.colorWidth * 2 - props.margin * 2}px`,
+        height: `${props.size - props.colorWidth * 2 - props.margin * 2}px`,
+        margin: `${props.margin}px`
+    }
+
     return (
         <div>
-            <div className={s.avatar}>
-                <div className={s.avatar__bg}>
-                    <img className={s.avatar__img} src={avatar} alt="avatar" />
+            <div style={Avatar}
+                 onMouseEnter={() => setBgColor({f: props.colors[0], s: props.colors[1]})}
+                 onMouseLeave={() => setBgColor({f: props.colors[1], s: props.colors[0]})}>
+                <div style={AvatarBg}>
+                    <img style={AvatarImg} src={avatar} alt={"avatar"}/>
                 </div>
             </div>
         </div>
